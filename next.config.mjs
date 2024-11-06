aab03fc4fefcecf1548fa2131acf367d135b32e3
niyas-softdev <softdev.niyas>
2024-11-06 16:25:36 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: 'dist', // Specify the output directory
+  };
+  
+  export default nextConfig;
+  
